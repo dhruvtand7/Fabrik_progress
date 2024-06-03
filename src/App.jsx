9894cfafd84fbe866eb641ea13f7da381e0e63f9
dhruvtand7 <dhrtandon@gmail.com>
2024-06-03 15:52:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef, useEffect } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber';
 import './App.css';
-import { OrbitControls } from '@react-three/drei';
-import { useLoader } from '@react-three/fiber';
+import { OrbitControls, Stats } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useRef } from 'react';
-import { Stats } from '@react-three/drei'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
-// Component to load the GLB file
-const Model = () => {
-  const glb = useLoader(GLTFLoader, '/sample/skibidi.glb');
-  const ref = useRef();
-
-  return <primitive ref={ref} object={glb.scene} scale={[3, 3, 3]} />;
-};
+// Component to load the GLB file with Draco compression
+function Scene() {
+  const path = "/sample/skibidi.glb"
+  const gltf = useLoader(GLTFLoader, path, (loader) => {
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/')
+    loader.setDRACOLoader(dracoLoader)
+})
+  return <primitive object={gltf.scene} />
+}
 
 export default function App() {
   return (
     <>
       <Canvas camera={{ position: [-8, 3, 8] }}>
-        <ambientLight intensity={20.0} />
+        <ambientLight intensity={1.0} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <Model />
+        <Scene />
         <OrbitControls />
         <Stats />
       </Canvas>
