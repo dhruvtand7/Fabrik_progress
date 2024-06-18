8d210e3112d1e0752dfdda2aeae36ddbcea206ae
dhruvtand7 <dhrtandon@gmail.com>
2024-06-18 12:08:21 +0530

    vercel deploment bug fix trial

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['firebase/storage', 'firebase/app'], // Add any other Firebase modules you use
+      external: /^firebase\//, // Match all Firebase modules
     },
   },
-})
+});
+
