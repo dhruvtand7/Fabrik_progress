7ec82400af6c162ca4f965bcc7f635af1f7ab6c7
dhruvtand7 <dhrtandon@gmail.com>
2024-06-19 09:40:23 +0530

    ignore(testing for deployment bug)

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'firebase/app': '/node_modules/firebase/app',
+      'firebase/storage': '/node_modules/firebase/storage',
+    },
+  },
   build: {
     rollupOptions: {
-      external: /^firebase\//, // Match all Firebase modules
+      external: ['firebase/app', 'firebase/storage'], // Ensure these are listed as externals
     },
   },
 });
